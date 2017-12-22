d70b5e84eb41effdc57fe8877d410d29fc5d1825
Sean McGroarty <mcgroas@tcd.ie>
2017-12-22 16:28:59 +0000

    added: MonadTrans definition

--- WriterT.hs
+++ WriterT.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies ,FlexibleInstances #-}
 module WriterT where
 
 import Control.Monad
-import Control.Monad.Trans
 import Data.Monoid
 
 newtype MyWriterT w m a = MyWriterT {  runMyWriterT :: m (a,w)  }
 
-class Monad m => (MonadWriter w m) where
+class MonadTrans t where
+        lift :: Monad m => m a -> t m a
+
+class (Monoid w, Monad m) => (MonadWriter w m) where
         tell :: (Monoid w, Monad m) => w -> MyWriterT w m ()
 
 instance (Monoid w, Monad m) => Functor (MyWriterT w m) where
@@ -24,10 +26,10 @@ instance (Monoid w, Monad m) => Monad (MyWriterT w m) where
                 (a',w') <- runMyWriterT (k a)
                 return (a',w `mappend` w')
 
-instance Monoid w => MonadTrans (MyWriterT w) where
+instance (Monoid w) => MonadTrans (MyWriterT w) where
         lift m = MyWriterT $ do
-                a <- m
-                return (a,mempty)
+                m' <- m
+                return (m',mempty)
 
 
 instance (Monoid w, Monad m) => MonadWriter w (MyWriterT w m) where
