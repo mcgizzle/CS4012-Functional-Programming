f0237deceb1d8ad7b3f70f13a5c048d8730c8a3d
Sean McGroarty <mcgroas@tcd.ie>
2017-12-22 15:10:52 +0000

    updated: throw and catch

--- ErrorT.hs
+++ ErrorT.hs
@@ -9,19 +9,29 @@ newtype MyErrorT m a = MyErrorT { runMyErrorT :: m (Either String a) }
 instance Monad m => Monad (MyErrorT m) where
         return = pure
         x >>= y = MyErrorT $ do
-                res <- x
+                res <- runMyErrorT x
                 case res of
-                  Right val -> return $ runMyErrorT $ y res 
+                  Right val -> runMyErrorT $ y val 
                   Left err  -> return $ Left err
 
 instance Monad m => Functor (MyErrorT m) where
         fmap = liftM 
 
 instance Monad m => Applicative (MyErrorT m) where
-        pure a = MyErrorT $ return $ Right a
+        pure = MyErrorT . return . Right
         (<*>) = ap
 
 instance MonadTrans MyErrorT where
-        lift m = MyErrorT $ liftM Right m
+        lift m = MyErrorT $ do
+                m' <- m
+                return $ Right m'
 
+throw :: Monad m => String -> MyErrorT m a
+throw = MyErrorT . return . Left
 
+catch :: Monad m => MyErrorT m a -> (String -> MyErrorT m a) -> MyErrorT m a 
+catch m c = MyErrorT $ do
+                m' <- runMyErrorT m
+                case m' of
+                  Left err -> runMyErrorT $ c err
+                  Right _  -> runMyErrorT m
