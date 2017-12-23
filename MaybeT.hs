4469045f3020de31b8f7a60bb0fbd90a7835db82
Sean McGroarty <mcgroas@tcd.ie>
2017-12-23 20:51:20 +0000

    added: MonadError

--- MaybeT.hs
+++ MaybeT.hs
@@ -1,13 +1,22 @@
+module MaybeT where
+
 import Control.Monad
-import Control.Monad.Trans
 
 newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
+
+class MonadTrans t where
+        lift :: Monad m => m a -> t m a
+
+class Monad m => MonadError m where
+        eFail :: m a
+        eHandle :: m a -> m a -> m a
 
 instance Monad m => Functor (MaybeT m) where
         fmap = liftM
 
 instance Monad m => Applicative (MaybeT m) where
         pure = MaybeT . return . Just
+        (<*>) = ap
 
 instance Monad m => Monad (MaybeT m) where
         return = pure
@@ -21,4 +30,12 @@ instance MonadTrans MaybeT where
         lift m = MaybeT $ do
                 m' <- m
                 return $ Just m'
+
+instance Monad m => MonadError (MaybeT m) where
+        eFail = MaybeT $ return Nothing
+        eHandle m1 m2 = MaybeT $ do
+                m1' <- runMaybeT m1
+                case m1' of
+                  Just _  -> return m1'
+                  Nothing -> runMaybeT m2
 
