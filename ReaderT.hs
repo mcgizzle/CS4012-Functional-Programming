1a186145646f25cabe7bb582f7e00fb3bd0fbbd2
Sean McGroarty <mcgroas@tcd.ie>
2017-12-23 20:44:49 +0000

    added: asks

--- ReaderT.hs
+++ ReaderT.hs
@@ -2,16 +2,22 @@
 module MyReaderT where
 
 import Control.Monad
-import Control.Monad.Trans
-import Control.Monad.Reader 
 
 newtype MyReaderT r m a = MyReaderT { runMyReaderT :: r -> m a }
+
+class MonadTrans t where
+        lift :: m a -> t m a
+
+class Monad m => (MonadReader r m) where
+        ask :: m r
+        asks :: (r -> a) -> m a
 
 instance Monad m => Functor (MyReaderT r m) where
         fmap = liftM
 
 instance Monad m => Applicative (MyReaderT r m) where
         pure a = MyReaderT $ \ _ -> return a
+        (<*>)  = ap
 
 instance Monad m => Monad (MyReaderT r m) where
         return = pure
@@ -24,3 +30,4 @@ instance MonadTrans (MyReaderT r) where
                 
 instance Monad m => MonadReader r (MyReaderT r m) where
         ask = MyReaderT $ \ r -> return r 
+        asks a = MyReaderT $ \ r -> return $ a r
