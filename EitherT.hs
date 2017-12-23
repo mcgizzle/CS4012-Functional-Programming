7bd5f6607fe1c5d098ff41f00f6fcbcb8b78b277
Sean McGroarty <mcgroas@tcd.ie>
2017-12-23 15:29:25 +0000

    added: MonadError

--- EitherT.hs
+++ EitherT.hs
@@ -1,15 +1,23 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies ,FlexibleInstances #-}
 module EitherT where
 
 import Control.Monad
-import Control.Monad.Trans
 
 newtype MyEitherT l m r = MyEitherT { runMyEitherT :: m (Either l r)}
+
+class MonadTrans t where
+        lift :: Monad m => m a -> t m a
+
+class (Show e, Monad m) => (MonadError e m) where
+        eFail :: e -> m a
+        eHandle :: m a -> (e -> m a) -> m a
 
 instance (Monad m) => Functor (MyEitherT l m) where
         fmap = liftM
 
 instance (Monad m) => Applicative (MyEitherT l m) where
-        pure = MyEitherT . pure . Right
+        pure  = MyEitherT . pure . Right
+        (<*>) = ap
 
 instance (Monad m) => Monad (MyEitherT l m) where
         return = pure
@@ -23,3 +31,11 @@ instance MonadTrans (MyEitherT l) where
         lift m = MyEitherT $ do
                 m' <- m
                 return $ Right m'
+
+instance (Show l, Monad m) => MonadError l (MyEitherT l m) where
+        eFail l = MyEitherT $ return $ Left l
+        eHandle m1 m2 = MyEitherT $ do
+                m1' <- runMyEitherT m1
+                case m1' of
+                  Right res -> return m1'
+                  Left err  -> runMyEitherT $ m2 err
